315517b5e63d0c564c0ab9d6c555581d2093974d
Yinghua Zeng [MVP] <28437644+sandytsang@users.noreply.github.com>
2019-07-09 17:24:39 +0300

    Create Autopilot.ps1

---
+++ FlowConnector/Autopilot.ps1
@@ -0,0 +1,42 @@
+Param (
+  [Parameter(Mandatory = $True, Position = 1, HelpMessage = "Please enter your email address")]
+  [ValidateNotNullorEmpty()]
+  [string]$YourEmailAddress,
+  
+  [Parameter(Mandatory = $True, Position = 2, HelpMessage = "Please choose company name:Customer1,Customer2 or Customer2")]
+  [ValidateSet('Customer1', 'Customer2', 'Customer3')]
+  [ValidateNotNullorEmpty()]
+  [string]$CompanyName,
+  
+  [Parameter(Mandatory = $False, Position = 3, HelpMessage = "Please enter assgin users's UserPrincipalName")]
+  [string]$assignedUserPrincipalName
+)
+
+
+
+$Serial = (Get-WmiObject  -Class Win32_BIOS).SerialNumber
+$DeviceHardwareData = (Get-WMIObject -Namespace root/cimv2/mdm/dmmap -Class MDM_DevDetail_Ext01 -Filter "InstanceID='Ext' AND ParentID='./DevDetail'").DeviceHardwareData
+
+$body = @"
+{
+ "orderIdentifier": $($CompanyName),
+ "serialNumber": $($Serial),
+ "hardwareIdentifier": $($DeviceHardwareData),
+ "EmailAddress": $($YourEmailAddress),
+ "assignedUserPrincipalName" : $($assignedUserPrincipalName)
+}
+"@
+
+#Change this to your Flow HTTP request url
+$Url = "https://prod-98.westeurope.logic.azure.com:443/workflows/38eebac34d2a46119f7fb3c165e73b5d/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=zQ71lxS4x8PzgO3eBObX4x_wgU_DCs8FNnucHJzwhTc"
+
+try
+{
+  #Make REST API call to flow
+  Invoke-RestMethod -uri $Url -Method Post -body $Body -ContentType 'application/json'
+  Write-Output "Please wait for email confirmation for approval to continue install this device"
+}
+catch
+{
+  Write-Error "$_.Exception.Message"
+}
